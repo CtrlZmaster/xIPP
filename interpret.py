c7567182e093092fe5f9eb3137f858f461304b15
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2019-03-29 13:27:07 +0100

    Classes draft in interpret

--- interpret.py
+++ interpret.py
@@ -1,2 +1,14 @@
-# This is only intended for testing test.php
-print("This is a stub.")
+"""
+Project: IPP Project 1
+File: interpret.py
+Title: IPPcode19 interpreter
+Description: This is an interpreter of IPPcode19 in XML representation
+Author: Michal Pospíšil (xpospi95@stud.fit.vutbr.cz)
+"""
+
+class frame
+
+class variable
+
+class instr_stream
+
