5d44c2e431132118277cae9e2b43db5a6a6f3935
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2019-04-10 20:11:53 +0200

    Interpreter supports numeric operations

--- interpret.py
+++ interpret.py
@@ -9,17 +9,118 @@ Author: Michal Pospíšil (xpospi95@stud.fit.vutbr.cz)
 import argparse
 import itertools
 import sys
-from xml.dom import minidom
+import re
 import xml.etree.ElementTree as xml_et
+import codecs
 
 
 class FrameSet:
     def __init__(self):
         self.local_frame_stack = []
         self.global_frame = Frame("global")
+        self.temporary_frame = None
+
+    def init_temporary_frame(self):
         self.temporary_frame = Frame("temporary")
 
+    def set_var(self, name):
+        exploded = re.split(r'@', name, maxsplit=1)
+        scope = exploded[0]
+        identifier = exploded[1]
+
+        if scope == "GF":
+            self.global_frame.set_var(identifier)
+
+        elif scope == "TF":
+            if self.temporary_frame is None:
+                print("Error: Temporary frame is not defined.", file=sys.stderr)
+                sys.exit(55)
+
+            self.temporary_frame.set_var(identifier)
+
+        elif scope == "LF":
+            try:
+                self.local_frame_stack[-1].set_var(identifier)
+            except IndexError:
+                print("Error: Local frame stack is empty.", file=sys.stderr)
+                sys.exit(55)
+
+        else:
+            print("Error: Unrecognized scope.", file=sys.stderr)
+            sys.exit(55)
+
+    def update_var(self, name, value, order):
+        exploded = re.split(r'@', name, maxsplit=1)
+        scope = exploded[0]
+        identifier = exploded[1]
+
+        if scope == "GF":
+            try:
+                self.global_frame.update_var(identifier, value)
+            except KeyError:
+                print("interpret.py:", order, ": Variable", identifier, "doesn't exist.")
+                sys.exit(54)
+
+        elif scope == "TF":
+            if self.temporary_frame is None:
+                print("Error: Temporary frame is not defined.", file=sys.stderr)
+                sys.exit(55)
+
+            try:
+                self.temporary_frame.update_var(identifier, value)
+            except KeyError:
+                print("interpret.py:", order, ": Variable", identifier, "doesn't exist.")
+                sys.exit(54)
+
+    def get_var(self, name, order):
+        exploded = re.split(r'@', name, maxsplit=1)
+        scope = exploded[0]
+        identifier = exploded[1]
+
+        if scope == "GF":
+            try:
+                retval = self.global_frame.get_var(identifier)
+            except KeyError:
+                print("interpret.py:", order, ": Variable", identifier, "doesn't exist in the global frame.",
+                      file=sys.stderr)
+                sys.exit(54)
+
+            return retval
+
+        elif scope == "TF":
+            if self.temporary_frame is None:
+                print("interpret.py:", order, ": Temporary frame is not defined.", file=sys.stderr)
+                sys.exit(55)
+
+            try:
+                retval = self.temporary_frame.get_var(identifier)
+            except KeyError:
+                print("interpret.py:", order, ": Variable", identifier, "doesn't exist in the temporary frame.",
+                      file=sys.stderr)
+                sys.exit(54)
+
+            return retval
+
+        elif scope == "LF":
+            try:
+                retval = self.local_frame_stack[-1].get_var(identifier)
+            except KeyError:
+                print("Error: Variable", identifier, "doesn't exist in this local frame.", file=sys.stderr)
+                sys.exit(54)
+            except IndexError:
+                print("Error: Local frame stack is empty.", file=sys.stderr)
+                sys.exit(55)
+
+            return retval
+        else:
+            print("Error: Unrecognized scope.", file=sys.stderr)
+            sys.exit(55)
+
     def push_temp(self):
+        if self.temporary_frame is None:
+            print("Error: Temporary frame is not defined.", file=sys.stderr)
+            sys.exit(55)
+
         self.local_frame_stack.append(self.temporary_frame)
         self.temporary_frame = None
 
@@ -36,6 +137,23 @@ class Frame:
         self.scope = scope
         self.vars = {}
 
+    def set_var(self, identifier):
+        self.vars[identifier] = Variable()
+
+    def update_var(self, identifier, value):
+        try:
+            self.vars[identifier].set_value(value)
+        except KeyError:
+            raise
+
+    def get_var(self, identifier):
+        try:
+            retval = self.vars[identifier]
+        except KeyError:
+            raise
+
+        return retval
+
 
 class Variable:
     def __init__(self):
@@ -44,13 +162,49 @@ class Variable:
 
     def set_value(self, value):
         self.value = value
-        self.type = type(value)
+        if isinstance(value, str):
+            self.type = "string"
+
+        if isinstance(value, int):
+            self.type = "int"
+
+        if value in {"bool@true", "bool@false"}:
+            self.type = "bool"
+
+        if value is "nil@nil":
+            self.type = "nil"
 
     def get_type(self):
-        return self.type
+        if self.type == "var":
+            if isinstance(self.value, str):
+                self.type = "string"
+
+            if isinstance(self.value, int):
+                self.type = "int"
+
+            if isinstance(self.value, bool):
+                self.type = "bool"
+
+            if self.value is None:
+                self.type = "nil"
+        else:
+            return self.type
 
     def get_value(self):
         return self.value
+
+
+def decode_escapes(s):
+    '''Helper function that reverses escaping done by xml.etree
+
+    Obtained from: https://stackoverflow.com/a/24519338
+    '''
+    escape_sequence_re = re.compile(r'\\[0-9]{3}', re.UNICODE | re.VERBOSE)
+
+    def decode_match(match):
+        return codecs.decode(match.group(0), 'unicode-escape')
+
+    return escape_sequence_re.sub(decode_match, s)
 
 
 class Program:
@@ -60,6 +214,8 @@ class Program:
         self.labels = {}                # Index names are labels and keys are instruction order values
         self.name = None
         self.description = None
+        self.frameset = FrameSet()
+        self.callstack = []             # List of return indices from call instructions to return instructions
 
     def extract_instructions(self):
         # Check program attributes
@@ -104,18 +260,19 @@ class Program:
             try:
                 order = int(order)
             except ValueError:
-                print("Error: Order attribute in instruction ", idx, " contains an invalid value.", file=sys.stderr)
+                print("interpret.py:", idx, "(in document order): Order attribute contains an invalid value.",
+                      file=sys.stderr)
                 sys.exit(32)
 
             if order < 1:
-                print("Error: Order attribute in instruction ", order, " is not bigger than 0.", file=sys.stderr)
+                print("interpret.py:", order, ": Order attribute is not bigger than 0.", file=sys.stderr)
                 sys.exit(32)
 
             ## Get opcode
             try:
-                opcode = instruction_attr.pop("opcode")
+                opcode = instruction_attr.pop("opcode").upper()
             except KeyError:
-                print("Error: Undefined opcode attribute in instruction ", order, '.', file=sys.stderr)
+                print("interpret.py:", order, ": Undefined opcode attribute in instruction ", order, '.', file=sys.stderr)
                 sys.exit(32)
 
             # Getting and checking arguments - at baseline, none are defined
@@ -144,7 +301,8 @@ class Program:
                           '.', file=sys.stderr)
                     sys.exit(32)
 
-                arg_text = argument.text  # Element without text returns has text set to None - treating here
+                ## Element without text returns has text set to None - treating here
+                arg_text = decode_escapes(argument.text)
                 if arg_text is None:
                     arg_text = ""
 
@@ -163,9 +321,25 @@ class Program:
 
             self.instructions[order] = Instruction(order, opcode, arg1, arg2, arg3, arg1_type, arg2_type, arg3_type)
 
+    def execute(self):
+        for instruction_key in self.instructions.keys():
+            # Passing program instance because instructions need to change frames, variables, etc.
+            self.instructions[instruction_key].execute(self)
+
 
 class Instruction:
+    """Instruction representation
+
+       Implements
+    """
+
+    accepted_const = {"int", "bool", "string", "nil"}
+
     def __init__(self, order, name, arg1, arg2, arg3, arg1_type, arg2_type, arg3_type):
+        """Instruction constructor
+
+           Takes
+        """
         self.order = order
         self.name = name
         self.argv = []
@@ -241,6 +415,214 @@ class Instruction:
                           file=sys.stderr)
                     sys.exit(53)
 
+        self.check_arg_syntax()
+
+    def check_arg_syntax(self):
+        pass
+
+    def read_var(self, program_instance, arg_idx, order):
+        retval = program_instance.frameset.get_var(self.argv[arg_idx], order)
+        if retval.type == "undefined":
+            print("Error: Variable", self.argv[arg_idx], "is undefined.", file=sys.stderr)
+            exit(56)
+
+        retval = retval.value
+
+        return retval
+
+    def read_symb(self, program_instance, arg_idx, order):
+        '''Helper function that reads expected symbol argument
+
+           @program_instance Instance of a program class - for getting variables
+           @arg_idx Index of the desired argument (1-3)
+           @return Value of a variable or a constant
+        '''
+        arg_idx = arg_idx - 1
+        retval = ""
+        if self.arg_types[arg_idx] == "var":
+            retval = self.read_var(program_instance, arg_idx, order)
+        elif self.arg_types[arg_idx] in self.accepted_const:
+            retval = self.argv[arg_idx]
+
+            if self.arg_types[arg_idx] == "int":
+                retval = int(retval)
+
+            if self.arg_types[arg_idx] == "nil":
+                retval = None
+
+            if self.arg_types[arg_idx] == "bool":
+                if retval == "true":
+                    retval = True
+
+                if retval == "false":
+                    retval = False
+
+        return retval
+
+    def execute(self, program_instance):
+        """Interpretation caller
+
+           This function calls instr_* functions that do the actual interpretation. Every instruction is independent,
+           so they can be executed separately.
+
+           @arg program_instance program instance is passed because some instructions change the control flow or modify
+                                 its member variables (e.g. frame stack)
+        """
+        eval("self.instr_" + self.name.lower() + "(program_instance)")
+
+    # 0 ARGUMENTS
+    def instr_createframe(self, program_instance):
+        program_instance.frameset.init_temporary_frame()
+
+    def instr_pushframe(self, program_instance):
+        program_instance.frameset.push_temp()
+
+    def instr_popframe(self, program_instance):
+        program_instance.frameset.pop_local()
+
+    def instr_return(self, program_instance):
+        pass
+
+    def instr_break(self, program_instance):
+        pass
+
+    # 1 ARGUMENT
+    def instr_defvar(self, program_instance):
+        program_instance.frameset.set_var(self.argv[0])
+
+    def instr_call(self, program_instance):
+        pass
+
+    def instr_pushs(self, program_instance):
+        pass
+
+    def instr_pops(self, program_instance):
+        pass
+
+    def instr_write(self, program_instance):
+        retval = self.read_symb(program_instance, 1, self.order)
+
+        if retval is True:
+            retval = "bool@true"
+
+        if retval is False:
+            retval = "bool@false"
+
+        if retval is None:
+            retval = "nil@nil"
+
+        print(retval, end='')
+
+    def instr_label(self, program_instance):
+        pass
+
+    def instr_jump(self, program_instance):
+        pass
+
+    def instr_exit(self, program_instance):
+        pass
+
+    def instr_dprint(self, program_instance):
+        pass
+
+    # 2 ARGUMENTS
+    def instr_move(self, program_instance):
+        value = self.read_symb(program_instance, 2, self.order)
+        program_instance.frameset.update_var(self.argv[0], value, self.order)
+
+    def instr_int2char(self, program_instance):
+        pass
+
+    def instr_read(self, program_instance):
+        pass
+
+    def instr_strlen(self, program_instance):
+        pass
+
+    def instr_type(self, program_instance):
+        pass
+
+    # 3 ARGUMENTS
+    def instr_add(self, program_instance):
+        arg2 = self.read_symb(program_instance, 2, self.order)
+        arg3 = self.read_symb(program_instance, 3, self.order)
+        if isinstance(arg2, int) and isinstance(arg3, int):
+            result = arg2 + arg3
+            program_instance.frameset.update_var(self.argv[0], result, self.order)
+        else:
+            print("interpret.py:", self.order, ": Variable must be of type int.", file=sys.stderr)
+            exit(53)
+
+    def instr_sub(self, program_instance):
+        arg2 = self.read_symb(program_instance, 2, self.order)
+        arg3 = self.read_symb(program_instance, 3, self.order)
+        if isinstance(arg2, int) and isinstance(arg3, int):
+            result = arg2 - arg3
+            program_instance.frameset.update_var(self.argv[0], result, self.order)
+        else:
+            print("interpret.py:", self.order, ": Variable must be of type int.", file=sys.stderr)
+            exit(53)
+
+    def instr_mul(self, program_instance):
+        arg2 = self.read_symb(program_instance, 2, self.order)
+        arg3 = self.read_symb(program_instance, 3, self.order)
+        if isinstance(arg2, int) and isinstance(arg3, int):
+            result = arg2 * arg3
+            program_instance.frameset.update_var(self.argv[0], result, self.order)
+        else:
+            print("interpret.py:", self.order, ": Variable must be of type int.", file=sys.stderr)
+            exit(53)
+
+    def instr_idiv(self, program_instance):
+        arg2 = self.read_symb(program_instance, 2, self.order)
+        arg3 = self.read_symb(program_instance, 3, self.order)
+        if isinstance(arg2, int) and isinstance(arg3, int):
+            if arg3 == 0:
+                print("interpret.py:", self.order, ": Division by zero.", file=sys.stderr)
+                exit(57)
+
+            result = arg2 + arg3
+            program_instance.frameset.update_var(self.argv[0], result, self.order)
+        else:
+            print("interpret.py:", self.order, ": Variable must be of type int.", file=sys.stderr)
+            exit(53)
+
+    def instr_lt(self, program_instance):
+        pass
+
+    def instr_gt(self, program_instance):
+        pass
+
+    def instr_eq(self, program_instance):
+        pass
+
+    def instr_and(self, program_instance):
+        pass
+
+    def instr_or(self, program_instance):
+        pass
+
+    def instr_not(self, program_instance):
+        pass
+
+    def instr_stri2int(self, program_instance):
+        pass
+
+    def instr_concat(self, program_instance):
+        pass
+
+    def instr_getchar(self, program_instance):
+        pass
+
+    def instr_setchar(self, program_instance):
+        pass
+
+    def instr_jumpifeq(self, program_instance):
+        pass
+
+    def instr_jumpifneq(self, program_instance):
+        pass
+
 
 class Args:
     @staticmethod
@@ -272,16 +654,28 @@ args = Args.parse()
 if args.source is not False:
     with open(args.source) as source_file:
         # Reading code from a file
-        xml_root = xml_et.parse(source_file).getroot()
+        try:
+            xml_root = xml_et.parse(source_file).getroot()
+        except xml_et.ParseError:
+            print("Error: Malformed XML.", file=sys.stderr)
+            sys.exit(31)
+
         program = Program(xml_root)
 else:
     # Reading code from stdin (source arg not set)
     source = sys.stdin.read()
-    xml_root = xml_et.fromstring(source)
+    try:
+        xml_root = xml_et.fromstring(source)
+    except xml_et.ParseError:
+        print("Error: Malformed XML.", file=sys.stderr)
+        sys.exit(31)
+
     program = Program(xml_root)
 
-# Now we have a program instance
+# Now we have a program instance with instructions
 program.extract_instructions()
 
+# Start the interpreter
+program.execute()
 
 exit(0)
